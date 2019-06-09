cda5741910d70ea32d0b994f39cc720ff9269aaa
yosh <yoshtalwar@gmail.com>
2019-06-09 14:24:17 +0700

    Use snake_case for import prefix

--- example/parse.dart
+++ example/parse.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
-import 'package:front_matter/front_matter.dart' as frontMatter;
+import 'package:front_matter/front_matter.dart' as front_matter;
 
 void main() async {
   var file = new File('example/hello-world.md');
   var contents = await file.readAsString();
 
   // Parse file contents.
-  var doc = frontMatter.parse(contents);
+  var doc = front_matter.parse(contents);
 
   print("The author is ${doc.data['author']}");
   print("The publish date is ${doc.data['author']}");
--- example/parse_file.dart
+++ example/parse_file.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
-import 'package:front_matter/front_matter.dart' as frontMatter;
+import 'package:front_matter/front_matter.dart' as front_matter;
 
 void main() async {
   // Read and parse a file from path.
-  var doc = await frontMatter.parseFile('example/hello-world.md');
+  var doc = await front_matter.parseFile('example/hello-world.md');
 
   print("The author is ${doc.data['author']}");
   print("The publish date is ${doc.data['date']}");
