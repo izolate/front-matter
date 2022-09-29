f39f3c8f9e2b6027dce125691984737c7b27d417
Maksim Lin <maks@manichord.com>
2022-09-30 09:59:32 +1000

    migrate to sound null safety

--- example/front_matter.dart
+++ example/front_matter.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'package:front_matter/front_matter.dart' as fm;
 
 // Example 1 - Parse a string.
-void example1() async {
+Future<void> example1() async {
   var file = File('example/hello-world.md');
   var fileContents = await file.readAsString();
 
@@ -14,7 +14,7 @@ void example1() async {
 }
 
 // Example 2 - Read a file and parse its contents.
-void example2() async {
+Future<void> example2() async {
   var doc = await fm.parseFile('example/hello-world.md');
 
   print("The author is ${doc.data['author']}");
